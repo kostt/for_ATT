5ac950a6d5a0de126163ccba20675841134c83f9
kostt <felix-mazur@mail.ru>
2017-08-23 19:20:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -31,17 +31,17 @@ final class myTest
    * метод install() создает таблицу, метод fill() заполняет таблицу
    */	
 
-	  function __construct() {
-	  	$this->install();
-	  	$this->fill();
-   }
+function __construct() {
+	$this->install();
+	$this->fill();
+}
 
  /**
    * Защищенный метод install() создает таблицу в БД
    */
 protected function install() 
 {
-$link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or die("Ошибка " . mysqli_error($link)); //Подключается к БД или выводит ошибку
+	$link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or die("Ошибка " . mysqli_error($link)); //Подключается к БД или выводит ошибку
 
  /**
    * Формируем запрос на создание таблицы myTable
@@ -51,26 +51,25 @@ $link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or d
    * script_result enum на выбор 3 значения
    */	
 
-$query ="CREATE TABLE myTable (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  script_name varchar(25) NOT NULL,
-  script_execution_time double(20,2) NOT NULL,
-  script_result enum('active','failed','success') DEFAULT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
+	$query ="CREATE TABLE myTable (
+  	id int(11) NOT NULL AUTO_INCREMENT,
+  	script_name varchar(25) NOT NULL,
+  	script_execution_time double(20,2) NOT NULL,
+  	script_result enum('active','failed','success') DEFAULT NULL,
+  	PRIMARY KEY (id)
+	) ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
 
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link) . ";<br>");  //Пробуем выпонить запрос
+	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link) . ";<br>");  //Пробуем выпонить запрос
 
-if($result)
-{
-    echo "Создание таблицы прошло успешно<br>";
-}
+	if($result){
+	echo "Создание таблицы прошло успешно<br>";
+	}
 
  /**
    * Пробуем создать таблицу, если успешно выводим сообщение и закрываем соединение с БД
    */	
  
-mysqli_close($link);
+	mysqli_close($link);
 }
 
  /**
@@ -78,48 +77,48 @@ mysqli_close($link);
    */
 protected function fill()
 {
-$link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or die("Ошибка " . mysqli_error($link));//Подключается к БД или выводит ошибку
+	$link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or die("Ошибка " . mysqli_error($link));//Подключается к БД или выводит ошибку
 
  /**
    * Подключаемся, показываем столбцы, где поле = script_result, чтобы, например, вписывать новые значения из базы, а не лезть в код
    */	
 
-$query = "SHOW COLUMNS FROM myTable WHERE Field = 'script_result'";//Формируем запрос для вывода значений из поля script_result
-$result = mysqli_query($link, $query); //Обрабатываем запрос
-$r = mysqli_fetch_array($result); //Выводим данные
+	$query = "SHOW COLUMNS FROM myTable WHERE Field = 'script_result'";//Формируем запрос для вывода значений из поля script_result
+	$result = mysqli_query($link, $query); //Обрабатываем запрос
+	$r = mysqli_fetch_array($result); //Выводим данные
 
 /**
 *@var array $matches массив (Убираем лишние символы)
 */
-preg_match("/^enum\(\'(.*)\'\)$/", $r['Type'], $matches);
+	preg_match("/^enum\(\'(.*)\'\)$/", $r['Type'], $matches);
 /**
 *@var array $array массив данных (разбиваем по ',' и заносим в массив)
 */
-$array = explode("','", $matches[1]);
+	$array = explode("','", $matches[1]);
 
-for($i=0;$i<=500;$i++) //Цикл на 500 значений
-{
+	for($i=0;$i<=500;$i++) //Цикл на 500 значений
+	{
 /**
 *@var string $tName создаём уникальные значения
 */
-$tName = md5(uniqid(""));
+	$tName = md5(uniqid(""));
 /**
 *@var string $tName обрезаем на 25 символов
 */
-$tName = substr($tName, 0, 25);
+	$tName = substr($tName, 0, 25);
 /**
 *@var integer $fTime создаём дробные числа рандомом
 */
-$fTime = round(lcg_value()*100000,2);
+	$fTime = round(lcg_value()*100000,2);
 /**
 *@var string $eCond рандомно присваиваем значения script_result
 */
-$eCond = $array[array_rand($array, 1)]; 
+	$eCond = $array[array_rand($array, 1)]; 
 
-$query = "INSERT INTO myTable (`script_name`,`script_execution_time`,`script_result`) VALUES ('$tName','$fTime','$eCond')"; //Формируем запрос для добавления новых полей
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link) . ";<br>");  //Пробуем выпонить запрос
-}
-echo "Заполнили таблицу<br>"; //Если успех - выводим сообщение
+	$query = "INSERT INTO myTable (`script_name`,`script_execution_time`,`script_result`) VALUES ('$tName','$fTime','$eCond')"; //Формируем запрос для добавления новых полей
+	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link) . ";<br>");  //Пробуем выпонить запрос
+	}
+	echo "Заполнили таблицу<br>"; //Если успех - выводим сообщение
 }
 
  /**
@@ -127,24 +126,20 @@ echo "Заполнили таблицу<br>"; //Если успех - вывод
    */
 static function get()
 {
-$link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or die("Ошибка " . mysqli_error($link)); //Подключается к БД или выводит ошибку
-$query = "SELECT * FROM myTable WHERE `script_result`='failed'"; //Выводим значения где script_result=failed
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link) . ";<br>"); //Пробуем выпонить запрос
-echo '<div class="container"><div class="row">'; //Создаем контейнер от bootstrap
-while($r = mysqli_fetch_array($result)){ //Значения из БД выводим в цикле
-echo '<div class="span4">'.$r['script_name'].'</div><div class="span4">'.$r['script_execution_time'].'</div><div class="span4">'.$r['script_result']."</div>"; //Помещаем значения в 3 колонки
+	$link = mysqli_connect(constant("LH"),constant("ROOT"), "", constant("DB")) or die("Ошибка " . mysqli_error($link)); //Подключается к БД или выводит ошибку
+	$query = "SELECT * FROM myTable WHERE `script_result`='failed'"; //Выводим значения где script_result=failed
+	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link) . ";<br>"); //Пробуем выпонить запрос
+	echo '<div class="container"><div class="row">'; //Создаем контейнер от bootstrap
+	
+	while($r = mysqli_fetch_array($result)){ //Значения из БД выводим в цикле
+	echo '<div class="span4">'.$r['script_name'].'</div><div class="span4">'.$r['script_execution_time'].'</div><div class="span4">'.$r['script_result']."</div>"; //Помещаем значения в 3 колонки
+	}
+	echo "</div></div>";//закрываем контейнер
 }
-echo "</div></div>";//закрываем контейнер
-
-}
-
 }
 
 $obj = new myTest(); //Создаем экземпляр класса
 $obj::get(); //Вызываем статический метод get для вывода данных
-
 ?>
-
-
 </body>
 </html>
